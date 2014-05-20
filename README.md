c09c2644b31c6510d0ce27ed93a7d8d111a9d6a5
Robert Foster <rfoster@refoster.com.au>
2014-05-20 16:26:07 +1000

    Added initial code for version 0.2 plugin

--- README.md
+++ README.md
@@ -2,3 +2,27 @@ UmbColourPicker
 ===============
 
 Spectrum Colour Picker for Umbraco 7
+
+Creates an "unbound" colour picker based on the Spectrum jquery plugin. By unbound, we mean that colours aren't pre-defined, allowing the user to choose a color of their own.
+
+For more information about Spectrum, go here: http://bgrins.github.io/spectrum/
+
+If you want to customise the settings for Spectrum, see the file in 
+
+/App_Plugins/SpectrumColorPicker/SpectrumColorPicker.controller.js
+
+and set the Spectrum options in the directive.
+
+TODO:
+-----
+1. Add Prevalues for various Spectrum options
+2. Remove Spectrum library in favour of the version now included in Umbraco.
+3. Create the Package on build (manifest is there, just need to add the build action and supporting libraries etc.)
+
+Updates:
+--------
+
+###Version 0.0.2:
+
+* Cleaned and simplified up Controller and removed Directive to make it more reliable.
+
---
+++ Source/SpectrumColourPicker/App_Plugins/SpectrumColorPicker/SpectrumColorPicker.controller.js
@@ -0,0 +1,29 @@
+﻿function spectrumColorPicker($scope, assetsService, angularHelper) {
+    assetsService.load(["/App_Plugins/SpectrumColorPicker/lib/spectrum/spectrum.js"]).then(
+        function () {
+            var elem = $('#colorpicker' + $scope.model.alias);
+            elem.spectrum({
+                showInitial: true,
+                preferredFormat: "name",
+                showInput: true,
+                showPalette: true,
+                palette: [],
+                //localStorageKey: "spectrum.SpectrumColorPickerPalette", // Any Spectrum with the same string will share selection
+                allowEmpty: true
+            });
+            elem.on('change', function () {
+                angularHelper.safeApply($scope, function () {
+                    // when a color is changed, update the model
+                    $scope.model.value = elem.val();
+                });
+            });
+            //$scope.model.onValueChanged = function (newVal, oldVal) {
+            //    elem.val($scope.model.value);
+            //};
+        });
+
+    //load the seperate css for the editor to avoid it blocking our js loading
+    assetsService.loadCss("/App_Plugins/SpectrumColorPicker/lib/spectrum/spectrum.css");
+}
+
+angular.module("umbraco").controller("ds.SpectrumColorPicker.Controller", spectrumColorPicker);
